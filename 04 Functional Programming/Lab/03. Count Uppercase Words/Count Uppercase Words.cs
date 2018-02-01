f4c4d146eade14a54df6d7304618a53e0a8235f8
Slavi <slaviana95@gmail.com>
2018-02-01 20:15:39 +0200

    FP Lab

---
+++ 04 Functional Programming/Lab/03. Count Uppercase Words/Count Uppercase Words.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+            words.Where(checker)
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
