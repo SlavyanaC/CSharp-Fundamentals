4ce1d5c308afa455aa0d3719901f108409033f22
Slavi <slaviana95@gmail.com>
2018-02-24 23:06:21 +0200

    06Animals

---
+++ C# OOP Basics/04Inheritance/Exercice/06Animals/Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+public class Animal
+{
+    private string name;
+    private int age;
+    private string gender;
+
+    public Animal(string name, int age, string gender)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Invalid input!");
+            name = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value.ToString()) || value < 0)
+                throw new ArgumentException("Invalid input!");
+            age = value;
+        }
+    }
+
+    public string Gender
+    {
+        get { return gender; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Invalid input!");
+
+            gender = value;
+        }
+    }
+
+    public virtual string ProduceSound()
+    {
+        return string.Empty;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendLine(this.GetType().Name);
+        builder.AppendLine(this.Name + " " + this.Age + " " + this.Gender);
+        builder.AppendLine(this.ProduceSound());
+
+        return builder.ToString().TrimEnd();
+    }
+}
---
+++ C# OOP Basics/04Inheritance/Exercice/06Animals/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06Animals
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    string type = command;
+                    string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    Animal animal;
+                    switch (type)
+                    {
+                        case "Dog":
+                            Dog dog = new Dog(info[0], int.Parse(info[1]), info[2]);
+                            animals.Add(dog);
+                            animal = dog;
+                            break;
+                        case "Frog":
+                            Frog frog = new Frog(info[0], int.Parse(info[1]), info[2]);
+                            animals.Add(frog);
+                            break;
+                        case "Cat":
+                                Cat cat = new Cat(info[0], int.Parse(info[1]), info[2]);
+                                animals.Add(cat);
+                            break;
+                        case "Kitten":
+                            Kitten kitten = new Kitten(info[0], int.Parse(info[1]));
+                            animals.Add(kitten);
+                            break;
+                        case "Tomcat":
+                            Tomcat tomcat = new Tomcat(info[0], int.Parse(info[1]));
+                            animals.Add(tomcat);
+                            break;
+                        default:
+                            throw new ArgumentException("Invalid input!");
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
