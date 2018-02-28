1df26c1b20d29084d7d411f31dfba7dea45a02b4
Slavi <slaviana95@gmail.com>
2018-02-28 22:42:55 +0200

    07FoodShortage

---
+++ C# OOP Basics/05InterfacesАndАbstraction/Exercice/07FoodShortage/Rebel.cs
@@ -0,0 +1,19 @@
+﻿public class Rebel : IName, IAge, IBuyer
+{
+    public string Name { get; private set; }
+
+    public int Age { get; private set; }
+
+    public int Food { get; private set; }
+
+    public Rebel(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public void BuyFood()
+    {
+        this.Food = 5;
+    }
+}
---
+++ C# OOP Basics/05InterfacesАndАbstraction/Exercice/07FoodShortage/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07FoodShortage
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Citizen> citizens = new List<Citizen>();
+            List<Rebel> rebels = new List<Rebel>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                if (tokens.Length == 4)
+                {
+                    string id = tokens[2];
+                    string birthdate = tokens[3];
+                    citizens.Add(new Citizen(name, age, id, birthdate));
+                }
+                else
+                {
+                    string group = tokens[2];
+                    rebels.Add(new Rebel(name, age));
+                }
+            }
+
+            string command = string.Empty;
+            int output = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (citizens.Any(c => c.Name == command))
+                {
+                    var citizen = citizens.FirstOrDefault(c => c.Name == command);
+                    citizen.BuyFood();
+                    output += citizen.Food;
+                }
+                else if (rebels.Any(r => r.Name == command))
+                {
+                    var rebel = rebels.FirstOrDefault(r => r.Name == command);
+                    rebel.BuyFood();
+                    output += rebel.Food;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
