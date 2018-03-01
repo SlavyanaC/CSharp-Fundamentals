10ff5210227b172e7154e33fa453e39a6ae18a5c
Slavi <slaviana95@gmail.com>
2018-03-01 21:25:07 +0200

    Edit on 08MilitaryElite

--- C# OOP Basics/05InterfacesАndАbstraction/Exercice/08MilitaryElite/SpecialisedSoldier.cs
+++ C# OOP Basics/05InterfacesАndАbstraction/Exercice/08MilitaryElite/SpecialisedSoldier.cs
@@ -2,24 +2,11 @@
 
 public abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
 {
-    private string[] validCorpses = { "Airforces", "Marines" };
-    private string corps;
-
     public SpecialisedSoldier(int id, string firstName, string lastName, double salary, string corps)
         : base(id, firstName, lastName, salary)
     {
         this.Corps = corps;
     }
 
-    public string Corps
-    {
-        get { return this.corps; }
-        set
-        {
-            if (validCorpses.Contains(value))
-            {
-                this.corps = value;
-            }
-        }
-    }
+    public string Corps { get;  }
 }
