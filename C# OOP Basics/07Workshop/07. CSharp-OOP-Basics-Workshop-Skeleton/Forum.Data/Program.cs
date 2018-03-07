7f9a9865b2b1a0706af10ec1da5725ef2e78f855
Slavi <slaviana95@gmail.com>
2018-03-07 16:04:59 +0200

    07Workshop First Part

---
+++ C# OOP Basics/07Workshop/07. CSharp-OOP-Basics-Workshop-Skeleton/Forum.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Forum.Data
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ C# OOP Basics/07Workshop/07. CSharp-OOP-Basics-Workshop-Skeleton/Forum.Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Forum.Models
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public User(int id, string username, string password, IEnumerable<int> posts)
+        {
+            this.Id = id;
+            this.Username = username;
+            this.Password = password;
+            this.Posts = new List<int>(posts);
+        }
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<int> Posts { get; set; }
+    }
+}
