a6b6034b895f4ca1828752a380bfb29b9b473aee
Slavi <slaviana95@gmail.com>
2018-03-22 23:45:31 +0200

    Generic P04-P08

---
+++ C# OOP Advanced/02Generics/Exercice/P06GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P06GenericCountMethodDouble
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+            for (int i = 0; i < count; i++)
+            {
+                box.Add(double.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(box.GetCountOfGreaterElements(double.Parse(Console.ReadLine())));
+        }
+    }
+}
---
+++ C# OOP Advanced/02Generics/Exercice/P07CustomList/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P07CustomList
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var input = string.Empty;
+            CustomList<string> customList = new CustomList<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] args = input.Split();
+                ExecuteCommand(customList, args);
+            }
+        }
+
+        private static void ExecuteCommand(CustomList<string> customList, string[] args)
+        {
+            var command = args[0];
+            switch (command)
+            {
+                case "Add":
+                    customList.Add(args[1]);
+                    break;
+                case "Remove":
+                    var index = int.Parse(args[1]);
+                    customList.Remove(index);
+                    break;
+                case "Contains":
+                    Console.WriteLine(customList.Contains(args[1]));
+                    break;
+                case "Swap":
+                    var firstIndex = int.Parse(args[1]);
+                    var secondIndex = int.Parse(args[2]);
+                    customList.Swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    Console.WriteLine(customList.GetCountOfGreaterElements(args[1]));
+                    break;
+                case "Max":
+                    Console.WriteLine(customList.Max());
+                    break;
+                case "Min":
+                    Console.WriteLine(customList.Min());
+                    break;
+                case "Print":
+                    Console.WriteLine(customList);
+                    break;
+            }
+        }
+    }
+}
