307e5ce7bd2f250b07c2439f562a6191705e70c8
Slavi <slaviana95@gmail.com>
2018-03-22 22:28:16 +0200

    Generics Exercice P01-P04

---
+++ C# OOP Advanced/02Generics/Exercice/P03GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P03GenericSwapMethodString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+            for (int i = 0; i < count; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+
+            var indices = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(indices[0], indices[1]);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
