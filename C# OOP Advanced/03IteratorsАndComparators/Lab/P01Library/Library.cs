9b048006c6bf06da880cbd2736151da4d325dc84
Slavi <slaviana95@gmail.com>
2018-03-25 19:43:51 +0300

    Edit

--- C# OOP Advanced/03IteratorsАndComparators/Lab/P01Library/Library.cs
+++ C# OOP Advanced/03IteratorsАndComparators/Lab/P01Library/Library.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
-public class Library 
+public class Library : IEnumerable<Book>
 {
     private List<Book> books;
 
     public Library(params Book[] books)
     {
         this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return this.books.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
