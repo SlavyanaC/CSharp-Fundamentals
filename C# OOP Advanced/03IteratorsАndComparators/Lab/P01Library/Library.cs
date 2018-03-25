146fe3d8f52427fa2a16bc06239779a2df7700e9
Slavi <slaviana95@gmail.com>
2018-03-25 18:26:59 +0300

    OOP Advanced Iterators and Comparators Lab 01-02

---
+++ C# OOP Advanced/03IteratorsАndComparators/Lab/P01Library/Library.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Library 
+{
+    private List<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+}
---
+++ C# OOP Advanced/03IteratorsАndComparators/Lab/P02LibraryIterator/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class Book
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = authors;
+    }
+
+    public string Title { get; set; }
+
+    public int Year { get; set; }
+
+    public IReadOnlyCollection<string> Authors { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+
+}
