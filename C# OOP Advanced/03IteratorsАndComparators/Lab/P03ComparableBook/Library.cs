74db121bf68e30bc77688995e6405f2024cd449d
Slavi <slaviana95@gmail.com>
2018-03-26 11:30:51 +0300

    Iterators and Comparators Lab

---
+++ C# OOP Advanced/03IteratorsАndComparators/Lab/P03ComparableBook/Library.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private List<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>();
+        }
+
+        public void Dispose() { }
+        public bool MoveNext() => ++currentIndex < this.books.Count;
+        public void Reset() => this.currentIndex = -1;
+        public Book Current => this.books[currentIndex];
+        object IEnumerator.Current => this.Current;
+    }
+}
