07423ac9431eee72a39b4179c823332232f42d8d
Slavi <slaviana95@gmail.com>
2018-03-30 17:03:21 +0300

    Reflection and Attributes Exercise P01-03

---
+++ C# OOP Advanced/04ReflectionAndAttributes/Exercise/P01HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            string inputLine = string.Empty;
+            while ((inputLine = Console.ReadLine()) != "HARVEST")
+            {
+                Type harvestingType = typeof(HarvestingFields);
+                FieldInfo[] fieldsInfo = harvestingType
+                    .GetFields(BindingFlags.Public |
+                    BindingFlags.NonPublic |
+                    BindingFlags.Instance |
+                    BindingFlags.Static);
+
+                string accessModifier = inputLine;
+                FieldInfo[] resultFields = GetFieldsWithSpecificModifier(accessModifier, fieldsInfo);
+                foreach (FieldInfo field in resultFields)
+                {
+                    string defauldModifier = field.Attributes.ToString().ToLower();
+                    string fieldModifier = defauldModifier == "family" ? "protected" : defauldModifier;
+                    Console.WriteLine($"{fieldModifier} {field.FieldType.Name} {field.Name}");
+                }
+            }
+        }
+
+        private static FieldInfo[] GetFieldsWithSpecificModifier(string accessModifier, FieldInfo[] fieldsInfo)
+        {
+            switch (accessModifier)
+            {
+                case "public":
+                    fieldsInfo = fieldsInfo.Where(f => f.IsPublic).ToArray();
+                    break;
+                case "private":
+                    fieldsInfo = fieldsInfo.Where(f => f.IsPrivate).ToArray();
+                    break;
+                case "protected":
+                    fieldsInfo = fieldsInfo.Where(f => f.IsFamily).ToArray();
+                    break;
+            }
+
+            return fieldsInfo;
+        }
+    }
+}
---
+++ C# OOP Advanced/04ReflectionAndAttributes/Exercise/P02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+
+            StringBuilder builder = new StringBuilder();
+            Type blackBoxType = typeof(BlackBoxInteger);
+            Object classInstance = Activator.CreateInstance(blackBoxType, true);
+
+            var methodsInfo = blackBoxType
+                .GetMethods(BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.NonPublic);
+
+            FieldInfo[] fieldsInfo = blackBoxType
+                .GetFields(BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.NonPublic);
+
+            var inputArgs = Console.ReadLine().Split('_');
+            while (inputArgs[0] != "END")
+            {
+                var value = int.Parse(inputArgs[1]);
+                methodsInfo.FirstOrDefault(m => m.Name == inputArgs[0])?.Invoke(classInstance, new object[] { value});
+
+                foreach (FieldInfo field in fieldsInfo)
+                {
+                    builder.AppendLine(field.GetValue(classInstance).ToString());
+                }
+
+                inputArgs = Console.ReadLine().Split('_');
+            }
+
+            Console.WriteLine(builder.ToString().TrimEnd());
+        }
+    }
+}
