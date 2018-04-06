1e17d58642e5397c0b7f781eae53817afd1d54c7
Slavi <slaviana95@gmail.com>
2018-04-06 17:07:40 +0300

    Unit tests edits and P03Iterator

---
+++ C# OOP Advanced/05UnitTesting/Exercise/P03Iterator/ListIterator.cs
@@ -0,0 +1,51 @@
+﻿namespace P03Iterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ListIterator
+    {
+        private List<string> data;
+        private int currentIndex;
+
+        public ListIterator(params string[] collection)
+        {
+            InitializeData(collection);
+            this.currentIndex = 0;
+        }
+
+        private void InitializeData(ICollection<string> collection)
+        {
+            if (collection == null || collection.Any(e => e == null))
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.data = new List<string>(collection);
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext() => this.currentIndex < this.data.Count - 1;
+
+        public string Print()
+        {
+            if (this.data.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            return this.data[this.currentIndex];
+        }
+    }
+}
---
+++ C# OOP Advanced/05UnitTesting/Exercise/P03Iterator/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace P03Iterator
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            ListIterator listIterator = null;
+
+            var inputLine = string.Empty;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var args = inputLine.Split();
+                var command = args[0];
+                switch (command)
+                {
+                    case "Create":
+                        listIterator = new ListIterator(args.Skip(1).ToArray());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            Console.WriteLine(listIterator.Print());
+                        }
+                        catch (InvalidOperationException ie)
+                        {
+                            Console.WriteLine(ie.Message);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
