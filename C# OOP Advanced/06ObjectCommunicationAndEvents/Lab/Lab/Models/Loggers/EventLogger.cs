387ee5d81a5687c3b9bd942b5e7eba8049dbd312
Slavi <slaviana95@gmail.com>
2018-04-08 22:22:51 +0300

    OOP Advanced Object Communication and Events

---
+++ C# OOP Advanced/06ObjectCommunicationAndEvents/Lab/Lab/Models/Loggers/EventLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class EventLogger : Logger
+{
+    public override void Handle(LogType type, string message)
+    {
+        switch (type)
+        {
+            case LogType.EVENT:
+                Console.WriteLine(type.ToString() + ": " + message);
+                break;
+        }
+    }
+}
---
+++ C# OOP Advanced/06ObjectCommunicationAndEvents/Lab/Lab/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace Object_Communication_and_Events_Lab
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Logger combatLog = new CombatLogger();
+            Logger eventLog = new EventLogger();
+
+            IAttackGroup attackGroup = new Group();
+
+            attackGroup.AddMember(new Warrior("Torsten", 10, combatLog));
+            attackGroup.AddMember(new Warrior("Rolo", 15, combatLog));
+
+            ITarget target = new Dragon("Transylvanian White", 200, 25, combatLog);
+
+            IExecutor executor = new CommandExecutor();
+
+            ICommand groupTarget = new GroupTargetCommand(attackGroup, target);
+            ICommand groupAttack = new GroupAttackCommand(attackGroup);
+        }
+    }
+}
