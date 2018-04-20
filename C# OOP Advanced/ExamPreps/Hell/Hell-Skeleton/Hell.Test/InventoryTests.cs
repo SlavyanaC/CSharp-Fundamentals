d1288fe9ca791e0b946db77cbb0006394b84e607
Slavi <slaviana95@gmail.com>
2018-04-20 14:23:00 +0300

    Edit (50Unit)

--- C# OOP Advanced/ExamPreps/Hell/Hell-Skeleton/Hell.Test/InventoryTests.cs
+++ C# OOP Advanced/ExamPreps/Hell/Hell-Skeleton/Hell.Test/InventoryTests.cs
@@ -3,56 +3,85 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 public class InventoryTests
 {
     private IInventory inventory = new HeroInventory();
-    IDictionary<string, IItem> commonItems;
-    IDictionary<string, IRecipe> recipeItems;
+    Dictionary<string, IItem> commonItems;
+    Dictionary<string, IRecipe> recipeItems;
 
-    IItem item = new CommonItem("Staff", 0, 10, 50, 100, 100);
+    private IItem firstItem;
+    private IItem secondItem;
+    private IRecipe recipeItem;
 
     [SetUp]
     public void SetUp()
     {
-        this.commonItems = GetCommonItems();
-        this.recipeItems = GetRecipeItems();
+        this.inventory = new HeroInventory();
     }
 
     [Test]
     public void AddsItemToCommonItems()
     {
-        this.inventory.AddCommonItem(this.item);
+        this.firstItem = new CommonItem("Knife", 0, 10, 0, 0, 30);
+        this.inventory.AddCommonItem(firstItem);
+
+        this.commonItems = GetCommonItems();
+
         Assert.That(this.commonItems.Count, Is.EqualTo(1));
     }
 
     [Test]
-    public void AddsRecipeToRecipes()
+    public void AddsRecipeToRecipeItems()
     {
+        List<string> reqItems = new List<string>() { "Knife", "Stick" };
+        this.recipeItem = new RecipeItem("Spear", 25, 10, 10, 100, 50, reqItems);
+        this.inventory.AddRecipeItem(recipeItem);
 
+        this.recipeItems = GetRecipeItems();
+
+        Assert.That(this.recipeItems.Count, Is.EqualTo(1));
     }
 
-    private IDictionary<string, IRecipe> GetRecipeItems()
+    [Test]
+    public void Test()
+    {
+        this.firstItem = new CommonItem("Knife", 0, 10, 0, 0, 30);
+        this.inventory.AddCommonItem(firstItem);
+
+        this.secondItem = new CommonItem("Stick", 0, 0, 10, 0, 5);
+        this.inventory.AddCommonItem(secondItem);
+
+        List<string> reqItems = new List<string>() { "Knife", "Stick" };
+        this.recipeItem = new RecipeItem("Spear", 25, 10, 10, 100, 50, reqItems);
+        this.inventory.AddRecipeItem(recipeItem);
+
+        this.commonItems = GetCommonItems();
+        this.recipeItems = GetRecipeItems();
+
+        Assert.That(this.commonItems.Count, Is.EqualTo(1));
+        Assert.That(this.recipeItems.Count, Is.EqualTo(1));
+    }
+
+    private Dictionary<string, IRecipe> GetRecipeItems()
     {
         FieldInfo itemsFromInventory = this.inventory.GetType()
                  .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
                  .FirstOrDefault(f => f.Name.Equals("recipeItems", StringComparison.OrdinalIgnoreCase));
 
-        IDictionary<string, IRecipe> recipesFromInventory = (IDictionary<string, IRecipe>)itemsFromInventory
+        Dictionary<string, IRecipe> recipesFromInventory = (Dictionary<string, IRecipe>)itemsFromInventory
             .GetValue(this.inventory);
 
         return recipesFromInventory;
     }
 
-    private IDictionary<string, IItem> GetCommonItems()
+    private Dictionary<string, IItem> GetCommonItems()
     {
         FieldInfo commonItemsFromInventory = this.inventory.GetType()
                  .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
                  .FirstOrDefault(f => f.Name.Equals($"{nameof(CommonItem)}s", StringComparison.OrdinalIgnoreCase));
 
-        IDictionary<string, IItem> itemsFromInventory = (IDictionary<string, IItem>)commonItemsFromInventory
+        Dictionary<string, IItem> itemsFromInventory = (Dictionary<string, IItem>)commonItemsFromInventory
             .GetValue(this.inventory);
 
         return itemsFromInventory;
