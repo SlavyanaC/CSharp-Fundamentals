37f00261f04bf1bd26d66dfe0f4c60f4ad1c292c
Slavi <slaviana95@gmail.com>
2018-04-26 18:57:21 +0300

    edit

---
+++ C# OOP Advanced/05UnitTesting/Exercise/P10TirePressureMonitoringSystem/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P10TirePressureMonitoringSystem
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
